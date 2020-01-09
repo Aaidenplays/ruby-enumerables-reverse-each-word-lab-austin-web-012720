2327e8e114e0a96bbe746c4cdf469cc9a1f0d30d
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-09 04:42:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def reverse_each_word(sentence1)
-  return_sentence = ''
+  return_sentence = []
   original_array = sentence1.split(" ")
     binding.pry
 
   original_array.each do |word|
-    return_sentence += word.reverse()
+    return_sentence << word.reverse()
   end
   return_sentence
 end
