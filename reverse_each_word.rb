70f645300db72500419b049ee072e707b16c2792
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-09 04:38:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def reverse_each_word(sentence1)
   new_string = ''
+    binding.pry
+
   sentence1.each do |word|
     new_string += word.reverse()
   end
-  binding.pry
   new_string
 end
